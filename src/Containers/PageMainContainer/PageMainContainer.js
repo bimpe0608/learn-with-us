f32a25a689be179bda5bf81dbd97e371109235bb
bimpe <adeyinminuadebimpe@gmail.com>
2023-08-04 14:57:22 +0100

    did mobile responsiveness

--- src/Containers/PageMainContainer/PageMainContainer.js
+++ src/Containers/PageMainContainer/PageMainContainer.js
@@ -20,10 +20,10 @@ const PageMainContainer = () => {
       <div className={classes.courseContainer}>
         <AvailableCourses>
           <div className={classes.frontendSection}>
-            <div>
+            <div className={classes.frontendImage}>
               <img src="https://res.cloudinary.com/drybwe5na/image/upload/v1690556097/Assets/WhatsApp_Image_2023-06-16_at_11.57_1_nsjrfk.svg" />
             </div>
-            <div>
+            <div className={classes.frontendText}>
               <h3>Learn Front End development</h3>
               <p>
                 Our carefully curated curriculum covers everything from HTML,
@@ -31,14 +31,16 @@ const PageMainContainer = () => {
                 tools. Gain hands-on experience and develop a robust foundation
                 that will set you apart in the competitive market.
               </p>
-              <Button />
+              <div>
+                <Button />
+              </div>
             </div>
           </div>
         </AvailableCourses>
 
         <AvailableCourses>
           <div className={classes.productDesignSection}>
-            <div>
+            <div className={classes.productDesignText}>
               <h3>Learn Product Design</h3>
               <p>
                 With our expert-led courses, you'll gain invaluable insights
@@ -47,19 +49,21 @@ const PageMainContainer = () => {
                 projects and real-world simulations, you'll develop the skills
                 to ideate, prototype, and bring your ideas to life.
               </p>
-              <Button />
+              <div>
+                <Button />
+              </div>
             </div>
-            <div>
+            <div className={classes.productDesignImage}>
               <img src="https://res.cloudinary.com/drybwe5na/image/upload/v1690556105/Assets/WhatsApp_Image_2023-06-16_at_11.57_5_rkzvfp.svg" />
             </div>
           </div>
         </AvailableCourses>
         <AvailableCourses>
           <div className={classes.dataAnalysisSection}>
-            <div>
+            <div className={classes.dataAnalysisImage}>
               <img src="https://res.cloudinary.com/drybwe5na/image/upload/v1690556102/Assets/WhatsApp_Image_2023-06-16_at_11.57_4_zsgece.svg" />
             </div>
-            <div>
+            <div className={classes.dataAnalysisText}>
               <h3>Learn Intermediate Level Data Analysis with Python</h3>
               <p>
                 Dive into real-world data sets and learn how to apply Python
@@ -67,7 +71,9 @@ const PageMainContainer = () => {
                 interpret data effectively. Gain practical experience through
                 interactive exercises and projects.
               </p>
-              <Button />
+              <div>
+                <Button />
+              </div>
             </div>
           </div>
         </AvailableCourses>
